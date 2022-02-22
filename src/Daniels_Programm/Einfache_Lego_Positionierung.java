0ac8f1b3021ead567e45c4682c81c5c332af2991
knpa1016 <knpa1016@EIT-ROBLAB-L02.ads.hs-karlsruhe.de>
2022-02-22 16:40:51 +0100

    --- automatic rebase to remote; old local SHA: fcdc571ae0496f469ea6edf46cfd39f7da1c4dd8 ---

--- src/Daniels_Programm/Einfache_Lego_Positionierung.java
+++ src/Daniels_Programm/Einfache_Lego_Positionierung.java
@@ -6,18 +6,18 @@ package Daniels_Programm;
 import javax.inject.Inject;
 import com.kuka.roboticsAPI.applicationModel.IApplicationData;
 import com.kuka.roboticsAPI.applicationModel.RoboticsAPIApplication;
-//import static com.kuka.roboticsAPI.motionModel.BasicMotions.*;
+import static com.kuka.roboticsAPI.motionModel.BasicMotions.*;
 import com.kuka.roboticsAPI.deviceModel.LBR;
-//import com.kuka.roboticsAPI.geometricModel.Frame;
-//import com.kuka.roboticsAPI.geometricModel.Tool;
-//import com.kuka.roboticsAPI.geometricModel.Workpiece;
-//import com.kuka.roboticsAPI.geometricModel.math.Transformation;
-//import com.kuka.roboticsAPI.motionModel.IMotion;
-//import com.kuka.roboticsAPI.motionModel.PTP;
-//import com.kuka.roboticsAPI.sensorModel.DataRecorder;
-//import com.kuka.roboticsAPI.sensorModel.ForceSensorData;
-//import com.kuka.task.ITaskLogger;
-//import com.sun.org.apache.xpath.internal.operations.Bool;
+import com.kuka.roboticsAPI.geometricModel.Frame;
+import com.kuka.roboticsAPI.geometricModel.Tool;
+import com.kuka.roboticsAPI.geometricModel.Workpiece;
+import com.kuka.roboticsAPI.geometricModel.math.Transformation;
+import com.kuka.roboticsAPI.motionModel.IMotion;
+import com.kuka.roboticsAPI.motionModel.PTP;
+import com.kuka.roboticsAPI.sensorModel.DataRecorder;
+import com.kuka.roboticsAPI.sensorModel.ForceSensorData;
+import com.kuka.task.ITaskLogger;
+import com.sun.org.apache.xpath.internal.operations.Bool;
 
 
 public class Einfache_Lego_Positionierung extends RoboticsAPIApplication {
@@ -46,10 +46,10 @@ public class Einfache_Lego_Positionierung extends RoboticsAPIApplication {
 	
 	public void run() {
 		
-		// lBR_iiwa_7_R800_1.move(ptp(0, 0, 0, 0, 0, 0, 0));
+		lBR_iiwa_7_R800_1.move(ptp(0, 0, 0, 0, 0, 0, 0));
 		
 		gripper.closeGripper();
-		gripper.openGripper();
+		//gripper.openGripper();
 
 		
 	}

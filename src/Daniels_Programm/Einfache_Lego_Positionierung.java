7e352aec8762f5e60fe87ad02ab484a8edb6e77a
knpa1016 <knpa1016@EIT-ROBLAB-L02.ads.hs-karlsruhe.de>
2022-02-28 17:54:19 +0100

    --- automatic rebase to remote; old local SHA: fc73e90c9bb420541ef4b6664955583d8d7eeb15 ---

--- src/Daniels_Programm/Einfache_Lego_Positionierung.java
+++ src/Daniels_Programm/Einfache_Lego_Positionierung.java
@@ -74,6 +74,13 @@ public class Einfache_Lego_Positionierung extends RoboticsAPIApplication {
 		lBR_iiwa_7_R800_1.move(ptp(0, 0, 0, 0, 0, 0, 0));
 		gripper.openGripper();
 		
+		lBR_iiwa_7_R800_1.move(ptp(getApplicationData().getFrame("/Kreis_Band_1")));
+		lBR_iiwa_7_R800_1.move(lin(getApplicationData().getFrame("/Kreis_Band_2")));
+		gripper.closeGripper();
+		lBR_iiwa_7_R800_1.move(lin(getApplicationData().getFrame("/Kreis_Band_1")));
+		gripper.openGripper();
+		
+		lBR_iiwa_7_R800_1.move(ptp(0, 0, 0, 0, 0, 0, 0));
 		
 		
 		// Schublade öffnen

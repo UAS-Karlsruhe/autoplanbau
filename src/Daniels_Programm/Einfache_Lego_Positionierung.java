53a083b63260a661ddce6ff88bbd248ea106b8cc
knpa1016 <knpa1016@EIT-ROBLAB-L02.ads.hs-karlsruhe.de>
2022-02-28 12:15:47 +0100

    --- automatic rebase to remote; old local SHA: a9cc1f74e31bdcfa9d81ef71377ab44a68c81b55 ---

--- src/Daniels_Programm/Einfache_Lego_Positionierung.java
+++ src/Daniels_Programm/Einfache_Lego_Positionierung.java
@@ -61,7 +61,7 @@ public class Einfache_Lego_Positionierung extends RoboticsAPIApplication {
 		lBR_iiwa_7_R800_1.move(lin(getApplicationData().getFrame("/Kreis_Schub_zu_2")));
 		lBR_iiwa_7_R800_1.move(lin(getApplicationData().getFrame("/Kreis_Schub_zu_3a")));
 		lBR_iiwa_7_R800_1.move(ptp(getApplicationData().getFrame("/Kreis_Schub_zu_4")));
-		lBR_iiwa_7_R800_1.move(lin(getApplicationData().getFrame("/Kreis_Schub_zu_5")));
+		lBR_iiwa_7_R800_1.move(lin(getApplicationData().getFrame("/Kreis_Schub_zu_5a")));
 		lBR_iiwa_7_R800_1.move(lin(getApplicationData().getFrame("/Kreis_Schub_zu_6")));
 		
 		lBR_iiwa_7_R800_1.move(ptp(0, 0, 0, 0, 0, 0, 0));

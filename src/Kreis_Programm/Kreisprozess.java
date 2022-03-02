bbe853c63a69f71f9210de8c9cc80969c1bdc844
knpa1016 <knpa1016@EIT-ROBLAB-L02.ads.hs-karlsruhe.de>
2022-03-02 15:51:32 +0100

    --- automatic rebase to remote; old local SHA: 160d80cab5d3838b2149e163dc09d2b58d1e556f ---

--- src/Kreis_Programm/Kreisprozess.java
+++ src/Kreis_Programm/Kreisprozess.java
@@ -153,11 +153,11 @@ public class Kreisprozess extends RoboticsAPIApplication {
 		lBR_iiwa_7_R800_1.move(ptp(0, 0, 0, 0, 0, 0, 0));
 		gripper.openGripper();
 		
-		lBR_iiwa_7_R800_1.move(ptp(getApplicationData().getFrame("/test4")));
+		lBR_iiwa_7_R800_1.move(ptp(getApplicationData().getFrame("/test1")));
 		logger.info("Schublade öffnen");
+		lBR_iiwa_7_R800_1.move(lin(getApplicationData().getFrame("/test4")));
+		gripper.closeGripper();
 		lBR_iiwa_7_R800_1.move(lin(getApplicationData().getFrame("/test5")));
-		//gripper.closeGripper();
-		//lBR_iiwa_7_R800_1.move(lin(getApplicationData().getFrame("/test4")));
 		gripper.openGripper();
 		
 		

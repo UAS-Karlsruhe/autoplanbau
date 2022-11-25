ce93b66298f406bc9d034a87ea4da783da6d9f5e
beto1020 <beto1020@EIT-ROBLAB-L03.ads.hs-karlsruhe.de>
2022-11-25 18:20:01 +0100

    --- automatic rebase to remote; old local SHA: bb317bbfa50813e69bd7e1e1c28bfd33e7949885 ---

--- src/application/AutoPlanBau.java
+++ src/application/AutoPlanBau.java
@@ -5,25 +5,27 @@ import com.kuka.roboticsAPI.applicationModel.RoboticsAPIApplication;
 import static com.kuka.roboticsAPI.motionModel.BasicMotions.*;
 
 import com.kuka.roboticsAPI.deviceModel.LBR;
+import com.kuka.roboticsAPI.geometricModel.Tool;
 import com.kuka.roboticsAPI.motionModel.PTP;
 
 public class AutoPlanBau extends RoboticsAPIApplication {
 	@Inject
 	private LBR lbr;
-	
-
+	private Tool TCP;
+ 
 			
 
 	@Override
 	public void initialize() {
 		// initialize your application here
+		TCP = getApplicationData().createFromTemplate("Lego_Sauger");
 	}
 
 	@Override
 	public void run() {
 		// your application execution starts here
-		//lbr.move(ptpHome());
-		
+		//lbr.move(ptpHome()); 
+		TCP.attachTo(lbr.getFlange());
 		
 		System.out.println("Move Bitch");
 		lbr.move(ptp(getApplicationData().getFrame("/A_Lego_Base/P1")).setJointVelocityRel(1));
@@ -37,7 +39,8 @@ public class AutoPlanBau extends RoboticsAPIApplication {
 		System.out.println("Move Bitch");
 		lbr.move(lin(getApplicationData().getFrame("/A_Lego_Base/P4")).setJointVelocityRel(1));
 		
-		
+		System.out.println("Move Bitch");
+		lbr.move(lin(getApplicationData().getFrame("/A_Lego_Base/P1")).setJointVelocityRel(1));
 		
 	}
 }

876cf0ae1a00dced9c06582e2d9ecb79cc26c1cd
beto1020 <beto1020@EIT-ROBLAB-L03.ads.hs-karlsruhe.de>
2022-11-25 15:56:14 +0100

    --- automatic rebase to remote; old local SHA: af96f2efeeb4f8acedc0e2ef3dd91c1ee15bd880 ---

--- src/application/AutoPlanBau.java
+++ src/application/AutoPlanBau.java
@@ -3,7 +3,9 @@ package application;
 import javax.inject.Inject;
 import com.kuka.roboticsAPI.applicationModel.RoboticsAPIApplication;
 import static com.kuka.roboticsAPI.motionModel.BasicMotions.*;
+
 import com.kuka.roboticsAPI.deviceModel.LBR;
+import com.kuka.roboticsAPI.motionModel.PTP;
 
 public class AutoPlanBau extends RoboticsAPIApplication {
 	@Inject
@@ -27,11 +29,7 @@ public class AutoPlanBau extends RoboticsAPIApplication {
 		System.out.println("Move Bitch");
 		lbr.move(ptp(getApplicationData().getFrame("P1")).setJointVelocityRel(0.1));
 		
-		System.out.println("Move Bitch");
-		lbr.move(ptp(getApplicationData().getFrame("P2")).setJointVelocityRel(0.1));
 		
-		System.out.println("Move Bitch");
-		lbr.move(ptp(getApplicationData().getFrame("P3")).setJointVelocityRel(0.1));
 		
 		
 	}

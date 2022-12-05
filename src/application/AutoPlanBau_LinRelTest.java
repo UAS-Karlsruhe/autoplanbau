4ef008674dc5387b03e58ae9f88a4d3a76f55463
beto1020 <beto1020@EIT-ROBLAB-L03.ads.hs-karlsruhe.de>
2022-12-05 17:41:00 +0100

    --- automatic rebase to remote; old local SHA: e2eec2dc3319a3dba47373a0dd61421878ae7db0 ---

--- src/application/AutoPlanBau_LinRelTest.java
+++ src/application/AutoPlanBau_LinRelTest.java
@@ -327,8 +327,8 @@ public class AutoPlanBau_LinRelTest extends RoboticsAPIApplication {
 			// Achtung änderungen in y und Rotation
 //			TCP.moveAsync(linRel(Transformation.ofDeg(BSB*(positionenx[i]),-(BSB*(positioneny[i])+0.8),0,90-rotation[i]-2,0,0),getApplicationData().getFrame("/A_Lego_Base/E1")).setBlendingCart(blendingCart));
 //			TCP.move(linRel(Transformation.ofDeg(0,0,(safePos+1-positionenz[i]),0,0,0),getApplicationData().getFrame("/A_Lego_Base/E1")).setJointVelocityRel(0.3));
-			TCP.getFrame("/A_Lego_Base/E1/vE1").moveAsync(linRel(Transformation.ofDeg(BSB*(positionenx[i]),-(BSB*(positioneny[i])+0.8),0,90-rotation[i]-2,0,0)).setBlendingCart(blendingCart));
-			TCP.getFrame("/A_Lego_Base/E1/vE1").move(linRel(Transformation.ofDeg(0,0,(safePos+1-positionenz[i]),0,0,0)));
+			TCP.getFrame("/A_Lego_Base/E1").moveAsync(linRel(Transformation.ofDeg(BSB*(positionenx[i]),-(BSB*(positioneny[i])+0.8),0,90-rotation[i]-2,0,0)).setBlendingCart(blendingCart));
+			TCP.getFrame("/A_Lego_Base/E1").move(linRel(Transformation.ofDeg(0,0,(safePos+1-positionenz[i]),0,0,0)));
 			
 			try {
 				
@@ -338,7 +338,7 @@ public class AutoPlanBau_LinRelTest extends RoboticsAPIApplication {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			TCP.getFrame("/A_Lego_Base/E1/vE1").move(linRel(Transformation.ofDeg(0,0,(-safePos),0,0,0)));
+			TCP.getFrame("/A_Lego_Base/E1").move(linRel(Transformation.ofDeg(0,0,(-safePos),0,0,0)));
 //			TCP.moveAsync(linRel(Transformation.ofDeg(0,0,-safePos,0,0,0),getApplicationData().getFrame("/A_Lego_Base/E1")).setBlendingCart(blendingCart).setJointVelocityRel(0.3));	
 //			
 		}

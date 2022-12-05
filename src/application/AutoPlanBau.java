5fd9419f5bea8e31270c5d78a028a406c0ea0966
beto1020 <beto1020@EIT-ROBLAB-L03.ads.hs-karlsruhe.de>
2022-12-05 16:37:29 +0100

    --- automatic rebase to remote; old local SHA: 0693a841953a525205efdf34b2026a0055d7e168 ---

--- src/application/AutoPlanBau.java
+++ src/application/AutoPlanBau.java
@@ -266,7 +266,7 @@ public class AutoPlanBau extends RoboticsAPIApplication {
 			
 			
 			if ((Stein[i] == 0) & (Zaehler4<=7)){
-					TCP.move(ptp(getApplicationData().getFrame("/A_Lego_Pal/Lego/vLego")).setJointVelocityRel(speed));
+					TCP.move(ptp(getApplicationData().getFrame("/A_Lego_Pal_test/Lego/vLego")).setJointVelocityRel(speed));
 					TCP.moveAsync(linRel(Transformation.ofDeg(PalAbsx*Zaehler4,0,0,-1,0,0),getApplicationData().getFrame("/A_Lego_Pal/Lego")).setBlendingCart(blendingCart));
 					TCP.move(linRel(Transformation.ofDeg(0,0,safePos,0,0,0),getApplicationData().getFrame("/A_Lego_Pal/Lego")).setJointVelocityRel(0.3));
 					try {
@@ -282,7 +282,7 @@ public class AutoPlanBau extends RoboticsAPIApplication {
 			
 			}
 			else if ((Stein[i] == 1)& (Zaehler8<=7)){		
-					TCP.move(ptp(getApplicationData().getFrame("/A_Lego_Pal/Lego/vLego")).setJointVelocityRel(speed));
+					TCP.move(ptp(getApplicationData().getFrame("/A_Lego_Pal_test/Lego/vLego")).setJointVelocityRel(speed));
 					// Y Achse -1 mm verschoben
 					TCP.moveAsync(linRel(Transformation.ofDeg(PalAbsx*Zaehler8,-(PalAbsy-1),-1,0,0,0),getApplicationData().getFrame("/A_Lego_Pal/Lego")).setBlendingCart(blendingCart));
 					TCP.move(linRel(Transformation.ofDeg(0,0,safePos,0,0,0),getApplicationData().getFrame("/A_Lego_Pal/Lego")).setJointVelocityRel(0.3));

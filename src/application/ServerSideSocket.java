fcd1747a51a5d56b1d57e09385bbf29d4e3b3db5
beto1020 <beto1020@EIT-ROBLAB-L03.ads.hs-karlsruhe.de>
2022-12-06 15:57:51 +0100

    --- automatic rebase to remote; old local SHA: e40f1c233e723798fd8e3c2e7fbe47685d8c58ca ---

--- src/application/ServerSideSocket.java
+++ src/application/ServerSideSocket.java
@@ -1,15 +1,30 @@
 package application;
 
+import javax.inject.Inject;
 
+import com.kuka.generated.ioAccess.VakuumIOGroup;
+import com.kuka.roboticsAPI.applicationModel.RoboticsAPIApplication;
+import static com.kuka.roboticsAPI.motionModel.BasicMotions.*;
 
-
+import com.kuka.roboticsAPI.deviceModel.LBR;
+//import com.kuka.roboticsAPI.geometricModel.AbstractFrame;
+import com.kuka.roboticsAPI.geometricModel.CartDOF;
+//import com.kuka.roboticsAPI.geometricModel.ObjectFrame;
+import com.kuka.roboticsAPI.geometricModel.Tool;
+import com.kuka.roboticsAPI.geometricModel.math.Transformation;
+//import com.kuka.roboticsAPI.motionModel.PTP;
+//import com.kuka.roboticsAPI.motionModel.RobotMotion;
+import com.kuka.roboticsAPI.motionModel.controlModeModel.CartesianImpedanceControlMode;
+import com.kuka.roboticsAPI.uiModel.ApplicationDialogType;
 
 import java.net.*;
 import java.io.*;
 
-public class ServerSideSocket { 
+public class ServerSideSocket extends RoboticsAPIApplication{ 
   public void run() {
-	try {
+	  getLogger().info("Starte Programm");
+	  
+	  try {
 		int serverPort = 6677;
 		ServerSocket serverSocket = new ServerSocket(serverPort);
 		serverSocket.setSoTimeout(5000000); 
@@ -17,13 +32,13 @@ public class ServerSideSocket {
 			System.out.println("Waiting for client on port " + serverSocket.getLocalPort() + "..."); 
 
 			Socket server = serverSocket.accept();
-			System.out.println("Just connected to " + server.getRemoteSocketAddress()); 
+			getLogger().info("Just connected to " + server.getRemoteSocketAddress()); 
 			PrintWriter toClient = 
 				new PrintWriter(server.getOutputStream(),true);
 			BufferedReader fromClient =
 				new BufferedReader(
 						new InputStreamReader(server.getInputStream()));
-			
+
 
 			String line = fromClient.readLine();
 

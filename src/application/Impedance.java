150566faa8ccaaf2214f92a72c23ac97c7bc713c
rajy1011 <rajy1011@EIT-ROBLAB-L02.ads.hs-karlsruhe.de>
2022-12-15 17:47:48 +0100

    --- automatic rebase to remote; old local SHA: 1a451db2f4b40e17dfbb43642ed9d4e7183bd122 ---

--- src/application/Impedance.java
+++ src/application/Impedance.java
@@ -20,9 +20,6 @@ public class Impedance extends RoboticsAPIApplication {
 	private static final int stiffnessZ = 2500;
 	private static final int stiffnessY = 700;
 	private static final int stiffnessX = 1500;
-
-	
-	
 	@Inject
 	private LBR lbr;
 	private static double[] startPosition=new double[]{0,offsetAxis2And4,0,offsetAxis2And4-Math.toRadians(90),0,Math.toRadians(90),0};

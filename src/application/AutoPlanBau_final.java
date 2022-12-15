e81e4a2165325e5a342939f7981c44d4da8da5b8
beto1020 <beto1020@EIT-ROBLAB-L03.ads.hs-karlsruhe.de>
2022-12-15 15:40:36 +0100

    --- automatic rebase to remote; old local SHA: 4cf357d2b2d0121cbc435ee160367c3afe70abbb ---

--- src/application/AutoPlanBau_final.java
+++ src/application/AutoPlanBau_final.java
@@ -129,7 +129,7 @@ public class AutoPlanBau_final extends RoboticsAPIApplication {
 				try {
 					int serverPort = 30001;
 					ServerSocket serverSocket = new ServerSocket(serverPort);
-					serverSocket.setSoTimeout(10000); 
+					serverSocket.setSoTimeout(100000); 
 					while(true) {
 						System.out.println("Waiting for client on port " + serverSocket.getLocalPort() + "..."); 
 
